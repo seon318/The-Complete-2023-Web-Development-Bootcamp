d887444ba9c68bc9732663302be5ba6389d096f6
seon318 <140936963+seon318@users.noreply.github.com>
2023-11-02 21:06:33 +0900

    section 23

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// var generateName = require("sillyname")
+
+import generateName from "sillyname"
+var sillyname = generateName();
+
+console.log(sillyname)
+
+import superheroes from "superheroes"
+const superHero = superheroes.random();
+
+console.log(`I am ${superHero}!`)
